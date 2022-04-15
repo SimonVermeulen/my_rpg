f99831be0df6797ae908da16d95f5237c74f3f2a
IKEA-luddes <user@DESKTOP-DGOC8RS.localdomain>
2022-04-15 06:30:43 +0200

    update .h documentation

--- include/engine/functions.h
+++ include/engine/functions.h
@@ -20,9 +20,32 @@ typedef struct execute_function_s {
     object_t *object;
 } execute_function_t;
 
+////////////////////////////////////////////////////////////
+/// \brief ajoute d'une nouvelle function qui s'executera à
+/// la prochaine debut de la boucle du jeu
+///
+/// Le prototype de la function c'est object_t *object, engine_t *engine
+///
+/// \param function la function qu'on veut executer
+///
+/// \param time le temps d'attente avant de l'executer
+/// en gros le game engine attendra jusqu'à que le temps que tu auras défini
+/// sera écoulé puis il l'executera sans perturbé le jeu pendant l'attente
+///
+/// \return renvoie si oui ou non il l'a pu l'ajouter
+///
+////////////////////////////////////////////////////////////
 sfBool add_function(event_functions_t function, float time, object_t *object,
     engine_t *engine);
+
+////////////////////////////////////////////////////////////
+/// Function système
+////////////////////////////////////////////////////////////
 int execute_functions(engine_t *engine);
+
+////////////////////////////////////////////////////////////
+/// Function système
+////////////////////////////////////////////////////////////
 int destroy_functions(engine_t *engine);
 
 #endif
