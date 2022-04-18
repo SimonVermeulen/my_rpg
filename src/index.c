b10bfd2ed1ceed684a30c923543544363006f7cb
Nouiri Adil <adil.nouiri@epitech.eu>
2022-04-18 17:50:05 +0200

    Add full intro_1

--- src/index.c
+++ src/index.c
@@ -26,8 +26,8 @@ int main(int argc, char **argv)
         !init_scenes_path("./scenes", engine))
         return 84;
     if (!set_const_scene("manager", engine) ||
-        !change_scene("intro_12-bot", engine) ||
-        !change_secondary_screen("intro_12-top", engine))
+        !change_scene("intro_1-bot", engine) ||
+        !change_secondary_screen("intro_1-top", engine))
         return 84;
     return open_game(engine, 60);
 }
