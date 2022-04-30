1f28adf18b196cc9379ab0139cbb6a7bff688b11
IKEA.kivik <kivik@ikea.eu>
2022-04-30 06:36:18 +0200

    update move_view

--- src/addons/move_view/view_action.c
+++ src/addons/move_view/view_action.c
@@ -25,26 +25,37 @@ static void move_the_view(sfView *bloc, vector_speed_t *destination,
     return;
 }
 
+static int view_apply(secondary_screen_t *secondary_screen, object_t *object,
+    list_t *move_view, int length)
+{
+    int *start = get_value_list(move_view, "start", 3);
+    list_t **path = get_value_list(move_view, "path", 10);
+    int *bloc = get_value_list(move_view, "bloc", 3);
+    vector_speed_t *destination = create_vector_speed(path[*start % length]);
+
+    if (*bloc == 1)
+        move_the_view(secondary_screen->bloc_1, destination, object->engine,
+            start);
+    else
+        move_the_view(secondary_screen->bloc_2, destination, object->engine,
+            start);
+    if (*start >= length)
+        set_active(true, seek_object_scene(object->actual_scene,
+        get_value_list(move_view, "enable", 4)), object->engine);
+}
+
 int tick_move_view(object_t *object, engine_t *engine)
 {
     list_t *move_view = get_addon_data("move_view", object);
     double *wait = get_value_list(move_view, "waitBeforeStart", 2);
     int *start = get_value_list(move_view, "start", 3);
-    list_t **path = get_value_list(move_view, "path", 10);
     int length = search_from_key(move_view, "path")->len;
     secondary_screen_t *secondary_screen = get_secondary_screen_data(engine);
-    int *bloc = get_value_list(move_view, "bloc", 3);
-    vector_speed_t *destination = create_vector_speed(path[*start % length]);
+
     *wait -= get_delta(engine);
     if (*wait > 0 || *start >= length)
         return 0;
     if (secondary_screen->second == NULL)
         return exit_game(engine, 84);
-    if (*bloc == 1)
-        move_the_view(secondary_screen->bloc_1, destination, engine, start);
-    else
-        move_the_view(secondary_screen->bloc_2, destination, engine, start);
-    if (*start >= length)
-        set_active(true, seek_object_scene(object->actual_scene,
-        get_value_list(move_view, "enable_object", 4)), engine);
+    view_apply(secondary_screen, object, move_view, length);
 }
